a06a5be347694651f902070e5b1d35c532a3943c
Steffenwolf <stefen.allen@gmail.com>
2019-03-26 14:51:40 -0600

    test

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,85 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <button @click='toggle'>Create</button>
+      <h1>Battle Bots</h1>
+      <button @click='battleBot'>Collection</button>
+      <hr>
+    </header>
+    
+    <section v-if='showCreate'>
+      <form action="">
+        <input type="text" placeholder="Bot Name" v-model='botName'>
+          <br>
+        <input type="number" min="0" step='1' placeholder="Attack Value" v-model='attackVal'>
+          <br>
+        <input type="number" min="0" step='1' placeholder="Health Value" v-model="healthVal">
+          <br>
+        <button @click='addBot'>Submit</button>
+        <button type='reset'>Clear</button>
+
+      </form>
+    </section>
+
+    <section v-else>
+      <h3>{{bot1}}</h3>
+      <h3>{{bot2}}</h3>
+      <hr>
+      {{botObj}}
+      <Bot v-for='(botObj, i) in allBots' :key='i' :botObj= 'botObj'/>
+
+
+    </section>
+
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Bot from './components/Bot'
 
 export default {
   name: 'app',
+
+  data: function(){
+    return{
+      showCreate: true,
+      botName: '',
+      attackVal: '',
+      healthVal: '',
+      allBots: [],
+      bot1: 'Contestant #1: Select a bot from below',
+      bot2: 'Contestant #2: Select a bot from below',
+      battleZone: [A, B]
+
+    }
+  },
+
+  methods: {
+    toggle: function() {
+      this.showCreate = !this.showCreate
+    },
+
+    addBot() {
+      const newBot = {botName: this.botName, attackVal: this.attackVal, healthVal: this.healthVal};
+      this.allBots.push(newBot);
+      this.botName = '';
+      this.attackVal = '';
+      this.healthVal = '';
+      this.toggle();
+    },
+
+    battleBot() {
+      var winner = this.battleZone[Math.floor(Math.random()*this.battleZone.length)]
+      return winner
+      console.log(winner)
+
+    }
+  },
+
   components: {
-    HelloWorld
+    Bot: Bot
+
   }
 }
 </script>
---
+++ src/components/Bot.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+    <h3>{{botObj.botName}}</h3>
+    <p>{{botObj.attackVal}}</p>
+    <p>{{botObj.healthVal}}</p>
+    <button>Select</button>
+    <button>Retire</button>
+
+
+  </section>
+
+</template>
+
+<script>
+export default {
+  props: ['botObj']
+}
+</script>
